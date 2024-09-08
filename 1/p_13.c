685a17a00c2e64a173170f8caee801222fe49f49
Achutha2207 <achutha22072004@gmail.com>
2024-09-08 21:13:02 +0530

    updated

--- 1/p_13.c
+++ 1/p_13.c
@@ -1,36 +1,61 @@
 #include<stdio.h>
 #include<math.h>
+#include<stdlib.h>
 #define FALSE 0
 #define TRUE 1
 
-int a[20];
+int board[20];
 
-int place(int k , int i){
-	int j ; 
+int isSafe(int currentRow, int currentColumn) {
+    int previousRow;
 
-	for(j=1;j<=k;j++){
-		if((a[j]==i)||(abs(a[j]-i)==abs(j-k)))
-			return FALSE;}
-	return TRUE;
+    // Check for conflicts with previously placed queens
+    for (previousRow = 1; previousRow < currentRow; previousRow++) {
+        if ((board[previousRow] == currentColumn) ||  // Same column
+         	 (abs(board[previousRow] - currentColumn) == abs(previousRow - currentRow))) {  // Same diagonal
+            return FALSE;
+        }
+    }
+    return TRUE;
 }
 
-int nqueens(int k , int n){
-	int i , a;
-	for(i=1;i<=n;i++){
-		if(place(k,i)) {a[k]=i;
-			if(k==n){
-				for(a=1;a<=n;a++){
-					printf("%d\t",a[a]);}
-				printf("\n");}
-			else{
-				nqueens(k+1,n);}}} return 0;}
-int main(){
-	int n ;
-	printf("Enter The Value Of n ");
-	scanf("%d",&n);
-	if(n<0||n==0||n==2||n==3){
-		printf("The Solution To %d Doesn't Eaists \n",n);
-		exit(1);}
-	printf("The Solution Of nQueens Problem Is \n");
-	nqueens(1,n);
-        return 0;}
+void solveNQueens(int currentRow, int boardSize) {
+    int currentColumn, row;
+
+    // Try to place a queen in every column of the current row
+    for (currentColumn = 1; currentColumn <= boardSize; currentColumn++) {
+        if (isSafe(currentRow, currentColumn)) {
+            board[currentRow] = currentColumn;  // Place queen
+
+            // If all queens are placed
+            if (currentRow == boardSize) {
+                // Print the board
+                for (row = 1; row <= boardSize; row++) {
+                    printf("%d\t", board[row]);
+                }
+                printf("\n");
+            } else {
+                // Recursively place the rest of the queens
+                solveNQueens(currentRow + 1, boardSize);
+            }
+        }
+    }
+}
+
+int main() {
+    int boardSize;
+
+    printf("Enter the value of n (number of queens): ");
+    scanf("%d", &boardSize);
+
+    if (boardSize <= 0 || boardSize == 2 || boardSize == 3) {
+        printf("The solution for %d does not exist.\n", boardSize);
+        return 1; // Terminate the program if no solution exists
+    }
+
+    printf("The solution(s) to the N-Queens problem is/are:\n");
+    solveNQueens(1, boardSize);
+
+    return 0;
+}
+
